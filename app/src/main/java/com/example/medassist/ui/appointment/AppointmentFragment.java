2ac5fa71057ac55a5634848ea8131ff207bbf83d
Ziin2 <htoo.kyawzin@gmail.com>
2025-04-01 16:08:38 +0800

    Added calendar view AGAIN

--- app/src/main/java/com/example/medassist/ui/appointment/AppointmentFragment.java
+++ app/src/main/java/com/example/medassist/ui/appointment/AppointmentFragment.java
@@ -1,33 +1,39 @@
 package com.example.medassist.ui.appointment;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import android.widget.CalendarView;
 import android.widget.TextView;
-
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
-
 import com.example.medassist.databinding.FragmentAppointmentBinding;
-import com.example.medassist.databinding.FragmentExerciseBinding;
-import com.example.medassist.ui.exercise.ExerciseViewModel;
 
 public class AppointmentFragment extends Fragment {
+
     private FragmentAppointmentBinding binding;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        AppointmentViewModel appointmentViewModel =
-                new ViewModelProvider(this).get(AppointmentViewModel.class);
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         binding = FragmentAppointmentBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
+        View view = binding.getRoot();
 
-        final TextView textView = binding.textAppointment;
-        appointmentViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+
+        binding.calendarView.setOnDateChangeListener(new android.widget.CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull android.widget.CalendarView view, int year, int month, int dayOfMonth) {
+                String selectedDate = dayOfMonth + "/" + (month + 1) + "/" + year;
+                binding.selectedDate.setText("Selected Date: " + selectedDate);
+            }
+        });
+
+        return view;
     }
 
     @Override
@@ -35,5 +41,4 @@ public class AppointmentFragment extends Fragment {
         super.onDestroyView();
         binding = null;
     }
-
 }
